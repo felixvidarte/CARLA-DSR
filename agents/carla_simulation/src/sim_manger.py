10ad52d057c862fcd172ccd733c2e92de844d817
robolab-kinova <you@example.com>
2022-06-30 12:34:11 +0200

    Working

--- agents/carla_simulation/src/sim_manger.py
+++ agents/carla_simulation/src/sim_manger.py
@@ -2,59 +2,95 @@ import carla
 import random
 import numpy as np
 import scipy.stats as scp
+import copy
+import cv2
+import time
+import weakref
+from threading import Lock
+mutex = Lock()
+
+# def carla_fun():
+#     ports = [2000, 20000, 40000, 60000, 63000]
+#     tm_ports = [8000, 16000, 24000, 30000, 35000]
+#     servers = []
+#     for p, t in list(zip(ports, tm_ports)):
+#         print(p, t)
+#         client = carla.Client("127.0.0.1", p)
+#         client.set_timeout(10.0)
+#         client.load_world("Arriba2")
+#         world = client.get_world()
+#         # world.get_spectator().set_transform(carla.Transform(carla.Location(25, 90, 376)))
+#         tm = client.get_trafficmanager(t)
+#         servers.append({'client': client,
+#                         'world': world,
+#                         'tm': tm
+#                         })
+#
+#     return servers
 
 
 class SimManager:
-    def __init__(self, data):
-        self.client = carla.client('localhost', data['port'])
-        self.client.load_world("Arriba2")
-        self.world = self.client.get_world()
-        self.world.get_spectator().set_transform(carla.Transform(carla.Location(25, 90, 376)))
-        self.tm = self.client.get_trafficmanager(data['tm_port'])
-        self.client.set_timeout(10.0)
-        print('Loading world...')
-        settings = self.world.get_settings()
-        settings.synchronous_mode = True
-        settings.fixed_delta_seconds = 0.1
-        settings.no_rendering_mode = True
-        # settings.substepping = True
-        # settings.max_substep_delta_time = 0.01
-        # settings.max_substeps = 10
-        self.world.apply_settings(settings)
-        self.spawn_points = self.world.get_map().get_spawn_points()
+    def __init__(self, actor_list, servers):
+        time1 = time.time()
+        self.client = servers['client']
+        self.loop = None
+        self.world = servers['world']
+        self.tm = servers['tm']
+        # settings = self.world.get_settings()
+        # settings.synchronous_mode = True
+        # settings.fixed_delta_seconds = 0.1
+        # settings.no_rendering_mode = True
+        # self.world.apply_settings(settings)
+
         self._blueprint_library = self.world.get_blueprint_library()
+
+        self.car_rgb_cams = {}
+        self.collision_sensor = None
 
         self.sim_register = {
             'brake': False,
-            'collision': False,
-            'actor_list': data["actor_list"],
+            'collision': {
+                'iscollision': False,
+                'timecollision': 0,
+                'actorcollision': 0
+            },
+            'actorList': copy.deepcopy(actor_list),
         }
 
-        self.tm.set_synchronous_mode(True)
-        self.tm.global_percentage_speed_difference(50)
+        # self.tm.set_synchronous_mode(True)
+        # self.tm.global_percentage_speed_difference(0)
+        self.real_time = 0
         self.max_velocity = 7
-
+        self.revision_time = 8.5
+        # self.destroy_actor()
+        self.world.tick()
         self.__load_actors()
 
     def __del__(self):
-        """Destructor"""
-        for actor in self.world.get_actors():
-            actor.destroy()
+        self.destroy_actor()
 
-    def world_tick(self, save_actors):
+    def world_tick(self, save_actors, real_time):#, show):
+        #print(self.world.get_actors())
+        #print(self.car_rgb_cams)
+        # self.mosaic()
+        self.real_time = real_time
         self.world.tick()
-        ego_id = self.sim_register["actor_list"][0]["carlaId"]
-        self.sim_register["brake"] |= self.world.get_actor(ego_id).get_control().brake > 0.5
+        # if show:
+        #     self.mosaic()
+        ego_id = self.sim_register["actorList"][0]["carlaID"]
+        self.sim_register["brake"] |= self.world.get_actor(ego_id).get_control().brake > 0.6
         if save_actors:
             self.__add_actor_pose()
-        self.real_time += self.world.get_settings().fixed_delta_seconds
 
     def get_results(self):
-        return self.sim_register
+        resultados = copy.deepcopy(self.sim_register)
+        #print(resultados)
+        return resultados
+
 
     '''PRIVATE AUX METHODS'''
     def __load_actors(self):
-        for actor in self.sim_register["actor_list"]:
+        for actor in self.sim_register["actorList"]:
             # print(actor['rol'])
             if actor['rol'] == 'ego_vehicle':
                 bp = self._blueprint_library.find('vehicle.tesla.model3')  # Nuestro vehiculo
@@ -65,57 +101,67 @@ class SimManager:
                 bp = random.choice(self.world.get_blueprint_library().filter('walker.*'))
             else:
                 print("Unknown actor")
-
-            spawn_point = carla.Transform(carla.Location(x=actor['fullPose'][-1][0], y=actor['fullPose'][-1][1], z=370), carla.Rotation(actor['fullPose'][-1][4], actor['fullPose'][-1][5], actor['fullPose'][-1][3]))  # Comprobar angulos CARLA-ROBOCOMP
+            spawn_point = carla.Transform(carla.Location(x=actor['initPose'][-1][0], y=actor['initPose'][-1][1], z=373), carla.Rotation(0, actor['initPose'][-1][2], 0))  # Comprobar angulos CARLA-ROBOCOMP
             # print(spawn_point)
-            carla_actors = []
-            try:
-                new_actor = self.world.spawn_actor(bp, spawn_point)
-                if bp.get_attribute(new_actor("role_name")) == "ego_vehicle":
-                    self.__set_ego_sensors(new_actor, 3)
-                self.__apply_control(new_actor, actor)
-                actor['carlaID'] = new_actor.id
-                carla_actors.append(new_actor)
+            # carla_actors = []
+            new_actor = self.world.spawn_actor(bp, spawn_point)
+            if bp.get_attribute("role_name") == "ego":
+                self.__set_ego_sensors(new_actor, 3)
+            self.__apply_control(new_actor, actor)
+            actor['carlaID'] = new_actor.id
+                # carla_actors.append(new_actor)
                 # print(self.carla_actors)
-            except Exception as e:
-                print(e)
+            # except Exception as e:
+            #     print(e)
+
+    def destroy_actor(self):
+        if self.collision_sensor is not None:
+            self.collision_sensor.destroy()
+            self.collision_sensor = None
+        for actor in self.world.get_actors():
+            if 'sensor' in actor.type_id:
+                pass
+            else:
+                actor.destroy()
+            # print(actor_destroy)
 
     def __apply_control(self, actor, actor_dict):
         if actor_dict['rol'] == 'ego_vehicle':
-            actor.set_autopilot(True)
-            # self.tm.keep_right_rule_percentage(actor, 100.0)
+            actor.apply_control(carla.VehicleControl(0.2))
+            actor.set_autopilot(True, self.tm.get_port())
+            self.tm.keep_right_rule_percentage(actor, 100.0)
             self.tm.ignore_walkers_percentage(actor, 100.0)
             self.tm.ignore_vehicles_percentage(actor, 100.0)
+            self.tm.vehicle_percentage_speed_difference(actor, self.__vehicle_control(actor_dict['initPose']))
             # self.tm.set_desired_speed(actor, 10)
         elif actor_dict['rol'] == 'vehicle':
-            actor.set_autopilot(True)
+            actor.set_autopilot(True, self.tm.get_port())
             self.tm.ignore_signs_percentage(actor, random.random() * 100)
-            self.tm.vehicle_percentage_speed_difference(actor, random.random() * 100)
+            self.tm.vehicle_percentage_speed_difference(actor, self.__vehicle_control(actor_dict['initPose']))
             self.tm.ignore_vehicles_percentage(actor, 100)
         elif actor_dict['rol'] == 'person':
-            print("PEOPLE APPLY CONTROL")
-            actor.apply_control(self.__person_control(actor_dict['fullPose']))
+            actor.apply_control(self.__person_control(actor_dict['initPose']))
 
     def __person_control(self, person_pose):
         pos = np.array(person_pose[-1])
-        direction = pos[5]
+        direction = pos[2]
         speed = 1.5
-        desv_direct = 20
+        desv_direct = 30
         if len(person_pose) > 1:
             pos_ant = np.array(person_pose[-2])
-            velocity = pos_ant - pos
-            current_speed = np.linalg.norm(velocity)/5
+            velocity = (pos_ant - pos) / self.revision_time
+            current_speed = np.linalg.norm(velocity)
             if current_speed > 0.1:
                 direction = np.rad2deg(np.arctan2(velocity[1], velocity[0]))
-                speed = np.linalg.norm(velocity) / 5
+                speed = current_speed
                 desv_direct = 10
-        dist_gauss_direction = scp.norm(direction, desv_direct)
-        dist_gauss_speed = scp.norm(speed, speed/3)
-        direction = dist_gauss_direction.rvs()
+            else:
+                speed *= 0.4
+        direction = self.__gaussian_distribution(direction, desv_direct)
+        speed = np.abs(self.__gaussian_distribution(speed, speed/3))
         [x, y] = [np.cos(np.rad2deg(direction)), np.sin(np.rad2deg(direction))]
         vector_direction = carla.Vector3D(x=x, y=y, z=0.0)
-        speed = dist_gauss_speed.rvs()
-        print(vector_direction,speed)
+        # print("People control", vector_direction, speed)
         return carla.WalkerControl(vector_direction, speed)
 
     def __vehicle_control(self, vehicle_pose):
@@ -123,14 +169,23 @@ class SimManager:
         current_velocity = self.max_velocity
         if len(vehicle_pose) > 1:
             pos_ant = np.array(vehicle_pose[-2])
-            velocity = np.linalg.norm(pos_ant-pos) / 5
+            velocity = np.linalg.norm(pos_ant-pos) / self.revision_time
             if velocity > 0.1:
                 current_velocity = velocity
+            else:
+                current_velocity = current_velocity / 2
         porcentage = (self.max_velocity - current_velocity)/self.max_velocity*100
-        porcentage = scp.norm(porcentage, np.abs(porcentage/3))
+        porcentage = self.__gaussian_distribution(porcentage, 10)
         return porcentage
 
+    def __gaussian_distribution(self, mean, std):
+        dist_gauss = scp.norm(mean, std)
+        valor = dist_gauss.rvs()
+        return valor
+
     def __set_ego_sensors(self, ego_vehicle, num_cams):
+        #print('SET_SENSORS')
+        weak_self = weakref.ref(self)
         # self.INPUT_WIDTH = 424
         # self.INPUT_HEIGHT = 240
         # cam_bp = self._blueprint_library.find('sensor.camera.rgb')
@@ -139,8 +194,8 @@ class SimManager:
         # cam_bp.set_attribute("fov", str(65))
         dimensiones_car = ego_vehicle.bounding_box.extent
         colision_sensor_location = carla.Location(dimensiones_car.x + 0.02, 0, dimensiones_car.z + 1)
-        # # In carla location is (pitch, yaw, roll) where pitch is y, yaw is z and roll is x
-        #
+        # In carla location is (pitch, yaw, roll) where pitch is y, yaw is z and roll is x
+
         # for i in range(num_cams):
         #     if i == 0:
         #         center_cam_location = carla.Location(dimensiones_car.x + 0.02, 0, dimensiones_car.z + 1)
@@ -168,16 +223,48 @@ class SimManager:
         #
         #         self.right_cam.listen(lambda image: self.sensor_rgb_callback(image, "RightCam"))
         collision_bp = self._blueprint_library.find('sensor.other.collision')
-        collision_sensor = self.world.spawn_actor(collision_bp, carla.Transform(location=colision_sensor_location, rotation=carla.Rotation()), attach_to=ego_vehicle,
-                                                  attachment_type=carla.AttachmentType.Rigid)
-        collision_sensor.listen(lambda collision: self.__collision_callback(collision))
+        self.collision_sensor = self.world.spawn_actor(collision_bp, carla.Transform(location=colision_sensor_location, rotation=carla.Rotation()), attach_to=ego_vehicle)
+        self.collision_sensor.listen(lambda collision: SimManager.__collision_callback(weak_self, collision))
 
     def __add_actor_pose(self): # TODO
-        for actor in self.sim_register["actor_list"]:
+        for actor in self.sim_register["actorList"]:
             actor_pose = self.world.get_actor(actor['carlaID']).get_transform()
-            pose = [actor_pose.location.x, actor_pose.location.y, actor_pose.location.z, actor_pose.rotation.roll, actor_pose.rotation.pitch, actor_pose.rotation.yaw]
-            actor['pose_list'].append(pose)
+            pose = [actor_pose.location.x, actor_pose.location.y, actor_pose.rotation.yaw]
+            actor['fullPose'].append(pose)
 
-    def __collision_callback(self, collision):  # TODO: review
-        # print("Collision with", collision.other_actor.type_id)
-        self.sim_register["collisions"] = True
+
+    @staticmethod
+    def __collision_callback(weak_self, collision):  # TODO: review
+        self = weak_self()
+        if not self:
+            return
+        if 'static' not in collision.other_actor.type_id and not self.sim_register['collision']['iscollision']:
+            #print("Collision with", collision.other_actor.type_id, collision.other_actor.id)
+            self.sim_register["collision"] = {
+                'iscollision': True,
+                'timecollision': self.real_time,
+                'actorcollision': 0
+            }
+            for actor in self.sim_register['actorList']:
+                if actor['carlaID'] == collision.other_actor.id:
+                    self.sim_register['collision']['actorcollision'] = actor['id']
+    #
+    # def sensor_rgb_callback(self, img, sensor_id):
+    #     print('PASA')
+    #     global mutex
+    #     mutex.acquire()
+    #     array = np.frombuffer(img.raw_data, dtype=np.dtype("uint8"))
+    #     array = np.reshape(array, (self.INPUT_WIDTH, self.INPUT_HEIGHT, 4))
+    #     array = array[:, :, :3]
+    #     self.car_rgb_cams[sensor_id] = array
+    #     print('FINCALLBACK')
+    #     mutex.release()
+    #
+    # def mosaic(self):
+    #     if len(self.car_rgb_cams.keys()) == 3:
+    #         v_f = cv2.hconcat([self.car_rgb_cams["LeftCam"], self.car_rgb_cams["CenterCam"]])
+    #         v_f = cv2.hconcat([v_f, self.car_rgb_cams["RightCam"]])
+    #         cv2.imshow("FrontCam", v_f)
+    #         cv2.waitKey(1)
+    #     else:
+    #         print("Wait for images")
--- agents/carla_simulation/src/specificworker.py
+++ agents/carla_simulation/src/specificworker.py
@@ -30,12 +30,16 @@ mutex = Lock()
 from rich.console import Console
 import interfaces as ifaces
 from genericworker import *
-# from sim_manager import *
 from sim_manger import *
-
-
+from multiprocessing import Process
+import multiprocessing
+import subprocess
+import json
 sys.path.append('/opt/robocomp/lib')
 console = Console(highlight=False)
+import threading
+import queue
+import psutil
 
 
 # If RoboComp was compiled with Python bindings you can use InnerModel in Python
@@ -44,18 +48,37 @@ console = Console(highlight=False)
 # import librobocomp_innermodel
 
 def carla_fun():
-    client = carla.Client("localhost", 2000)
-    client.set_timeout(10.0)
-    client.load_world("Arriba2")
-    world = client.get_world()
-    world.get_spectator().set_transform(carla.Transform(carla.Location(25, 90, 376)))
-    tm = client.get_trafficmanager(8000)
+    ports = [2000, 4000, 10000, 15000, 20000, 40000, 60000, 63000]
+    tm_ports = [8000, 12000, 14000, 16000, 22000, 24000, 30000, 35000]
+    port_list = list(zip(ports, tm_ports))
+    z = 8
+    servers = []
+    for i in range(0, z):
+        p, t = port_list[i]
+        print(p, t)
+        client = carla.Client("127.0.0.1", p)
+        client.set_timeout(10.0)
+        client.load_world("Arriba2")
+        world = client.get_world()
+        settings = world.get_settings()
+        settings.synchronous_mode = True
+        settings.fixed_delta_seconds = 0.1
+        settings.no_rendering_mode = True
+        world.apply_settings(settings)
+        print(world.get_actors())
+        # world.get_spectator().set_transform(carla.Transform(carla.Location(25, 90, 376)))
+        tm = client.get_trafficmanager(t)
+        # tm.set_synchronous_mode(True)
+        servers.append({'client': client,
+                        'world': world,
+                        'tm': tm
+                        })
 
-    return [client, world, tm]
+    return servers
 
 
 class SpecificWorker(GenericWorker):
-    def __init__(self, proxy_map, startup_check=True):
+    def __init__(self, proxy_map, startup_check=True, servers=carla_fun()):
         super(SpecificWorker, self).__init__(proxy_map)
         self.Period = 100
         if startup_check:
@@ -63,10 +86,8 @@ class SpecificWorker(GenericWorker):
         else:
             self.timer.timeout.connect(self.compute)
             self.timer.start(self.Period)
-
         self.time_ini = 0.0
         self.n_simulations = 5
-
         self.cond_simulation = None
 
         self.results = {
@@ -76,74 +97,134 @@ class SpecificWorker(GenericWorker):
             'time': 0
         }
 
-        self.revision_time = 5
-
-        self.sim_params = [
-            {  # TODO: crear una por cada simulación
-                'port': 2000,
-                'tm_port': 8000,
-                'actor_list': [],
-            },
-
-            {  # TODO: crear una por cada simulación
-                'port': 20000,
-                'tm_port': 10000,
-                'actor_list': [],
-            }
-        ]
-
+        self.revision_time = 3
+        self.servers = servers
+        # print(self.servers)
         self.fixed_delta_seconds = 0.1
-
 
     def __del__(self):
         """Destructor"""
-        for actor in self.world.get_actors():
-            actor.destroy
+        # for actor in self.world.get_actors():
+        #     actor.destroy()
+        return True
 
     def setParams(self, params):
         return True
 
     @QtCore.Slot()
     def compute(self):
-        '''
-        compute:
-            Esperar por las condiciones de las simulaciones nuevas
-            while(simular)
-                tick de todas las simulaciones
-            resultados de las simulaciones
-            pasar a la interfaz
-        '''
-
+        my_queue = queue.Queue()
         if self.cond_simulation is not None:
 
-            # Creación de las simulaciones nuevas
+            def storeInQueue(f):
+                def wrapper(*args):
+                    my_queue.put(f(*args))
+                return wrapper
+
+            @storeInQueue
+            def multitask(actor_list,servers, duration, revision_time, fixed_delta, name, n_sim):
+                # print(name)
+                cpu_usage = []
+                compute_time = []
+                j = 0
+                full_results = []
+                while j < n_sim:
+                    real_time = 0.0
+                    time_ini1 = time.time()
+                    s = SimManager(actor_list, servers)
+                    #print("time initttt", time.time()-time_ini1)
+                    i = 1
+                    save_actor = False
+                    # k = 0
+                    while real_time < duration:
+                        time_ini = time.time()
+                        # if name == 'sim0' and real_time >= k:
+                        #     cpu_usage.append(psutil.cpu_percent())
+                        #     k += 1
+                        # print(real_time)
+                        if real_time > revision_time * i:
+                            save_actor = True
+                            i += 1
+                        # if s.sim_register['collision']['iscollision'] == True:
+                        #     s.sim_register['collision']['timecollision'] = real_time
+                        s.world_tick(save_actor, real_time)
+                        save_actor = False
+                        real_time += fixed_delta
+                        compute_time.append(time.time()-time_ini)
+                    full_results.append(s.get_results())
+                    del s
+                    j += 1
+                # if name == 'sim0':
+                #     print("CPU usage", sum(cpu_usage)/len(cpu_usage))
+                #     print("Total", cpu_usage)
+                return full_results
+            # @storeInQueue
+            # def multitask(actor_list,servers, duration, revision_time, fixed_delta, name):
+            #     # print(name)
+            #     cpu_usage = []
+            #     compute_time = []
+            #     real_time = 0.0
+            #     time_ini1 = time.time()
+            #     s = SimManager(actor_list, servers)
+            #     #print("time initttt", time.time()-time_ini1)
+            #     i = 1
+            #     save_actor = False
+            #     while real_time < duration:
+            #         time_ini = time.time()
+            #         if name == 'sim0' and real_time >= 1:
+            #             cpu_usage.append(psutil.cpu_percent())
+            #         # print(real_time)
+            #         if real_time > revision_time * i:
+            #             save_actor = True
+            #             i += 1
+            #         # if s.sim_register['collision']['iscollision'] == True:
+            #         #     s.sim_register['collision']['timecollision'] = real_time
+            #         s.world_tick(save_actor, real_time)
+            #         save_actor = False
+            #         real_time += fixed_delta
+            #         compute_time.append(time.time()-time_ini)
+            #     results = s.get_results()
+            #     del s
+            #     if name == 'sim0':
+            #         print("CPU usage", sum(cpu_usage)/len(cpu_usage))
+            #         # print("Compute_time", sum(compute_time) / len(compute_time))
+            #     return results
+
             self.time_ini = time.time()
-            duration = self.cond_simulation["duration"]
-            simulations = []
-            real_time = 0
-            for i in range(self.cond_simulation["n_simulations"]):
-                simulations.append(SimManager(self.sim_params[i]))
+            c = 0
+            results = []
+            tim1 = time.time()
+            for i in self.servers:
+                t1 = threading.Thread(name=f'sim{c}', target=multitask, args=(self.cond_simulation['actorList'],i,self.cond_simulation["duration"], self.revision_time, self.fixed_delta_seconds, f'sim{c}', 1))
+                t1.start()
+                #t1.join()
+                print("EMPIEZA HILO", f'sim{c}' , time.time() - tim1)
+                # results.append(my_queue.get())
+                # if c == len(self.servers):
+                #     t1.join()
+                c += 1
+            for _ in self.servers:
+                aux = my_queue.get()
+                for i in aux:
+                    results.append(i)
+                # results.append(my_queue.get())
+            # results.append(my_queue.get())
+            # for job in jobs:
+            #     job.kill()
 
-            # Simular
-            while real_time < duration:
-                for s in simulations:
-                    s.world_tick()
-                real_time += self.fixed_delta_seconds
-
-            #Obtener resultados
-            results = [s.get_results for s in simulations]
+            # print(results)
             self.store_results(results)
-
-        else:
-            print("Waiting for conditions...")
+        # else:
+            # print("Waiting for conditions...")
 
     def store_results(self, results):
         self.cond_simulation = None
-        self.results['fullResult'] = results
-        self.results['time'] = time.time() - self.time_ini
-        self.results['valid'] = True
-        self.results['n_simulation'] = self.n_simulations
-        print(self.results)
+        self.results = {
+            'valid': True,
+            'n_simulation ': self.n_simulations,
+            'fullResult': results,
+            'time': time.time() - self.time_ini
+        }
 
     def startup_check(self):
         QTimer.singleShot(200, QApplication.instance().quit)
@@ -152,44 +233,49 @@ class SpecificWorker(GenericWorker):
     def Carla_getState(self):
         ret = ifaces.RoboCompCarla.Results()
         if self.results['valid']:
-            print("RESULTADO VALIDO")
-            ret.time = time.time() - self.time_ini
+            # print("RESULTADO VALIDO")
+            # print(self.results['time'])
+            ret.time = self.results['time']
+            print("Tiempo de simulacion", ret.time)
             ret.nsimulation = self.n_simulations
             ret.valid = self.results['valid']
             ret.fullresult = ifaces.Fullresults()
             for r in self.results['fullResult']:
                 result = ifaces.RoboCompCarla.Simresult()
-                result.colision = r['collision']
-                result.isbrake = r['isBrake']
+                # collision = ifaces.RoboCompCarla.Collision(r['collision']['iscollision'],r['collision']['iscollision'],r['timecollision']['actorcollision'])
+                collision = ifaces.RoboCompCarla.Collision(r['collision']['iscollision'], r['collision']['timecollision'], r['collision']['actorcollision'])
+                result.collision = collision
+                result.isbrake = r['brake']
                 result.actorlist = ifaces.Actors()
                 for actor in r['actorList']:
-                    act = ifaces.RoboCompCarla.Actor(actor['id'], actor['carlaID'], ifaces.Fullposedata(), actor['rol'])
+                    act = ifaces.RoboCompCarla.Actor(actor['id'], actor['carlaID'], ifaces.Fullposedata(), ifaces.Fullposedata(), actor['rol'])
                     for pose in actor['fullPose']:
-                        act.pose.append(ifaces.RoboCompCarla.Posedata(pose[0], pose[1], pose[2], pose[3], pose[4], pose[5]))
+                        act.pose.append(ifaces.RoboCompCarla.Posedata(pose[0], pose[1], 0, 0, 0, pose[2]))
                     result.actorlist.append(act)
                 ret.fullresult.append(result)
             self.results['valid'] = False
         return ret
 
     def Carla_setSimulationParam(self, condini):
-        if not self.cond_simulation:
+        if not self.cond_simulation and condini.duration > 8:
             actor_list = []
             for actor in condini.actorlist:
                 act = {
                     'id': actor.id,
                     'carlaID': actor.carlaid,
+                    'initPose': [],
                     'fullPose': [],
                     'rol': actor.rol
                 }
-                for pose in actor.pose:
-                    act['fullPose'].append([pose.tx, pose.ty, pose.tz, pose.rx, pose.ry, pose.rz])
+                for pose in actor.initpose:
+                    act['initPose'].append([pose.tx, pose.ty, pose.rz])
                 actor_list.append(act)
             self.cond_simulation = {
                 "n_simulation": condini.nsimulation,
                 "duration": condini.duration,
                 "actorList": actor_list
             }
-            print("New simulations arrive:", self.cond_simulation)
-        else:
-            print("Simulating....")
+            # print("New simulations arrive:", self.cond_simulation["n_simulation"])
+        # else:
+            # print("Simulating....")
 
